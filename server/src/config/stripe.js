1fbf4d15b99c889543b61336ef39cdaa201d390b
habtamubaye-devsec <habtamubaye2127gmail.com>
2024-10-23 07:04:00 +0000

    Update: src/config/stripe.js - commit 4

--- server/src/config/stripe.js
+++ server/src/config/stripe.js
@@ -6,3 +6,4 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Auto change for Tue Oct 22 2024 03:00:00 GMT+0300 (East Africa Time)
 // Auto change for Tue Oct 22 2024 03:00:00 GMT+0300 (East Africa Time)
+// Auto change for Wed Oct 23 2024 03:00:00 GMT+0300 (East Africa Time)
