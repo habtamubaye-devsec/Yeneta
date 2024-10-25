9a16867090e87694c10b0e4da35cce953858ff63
habtamubaye-devsec <habtamubaye2127gmail.com>
2024-10-25 07:01:00 +0000

    Update: src/middlewares/upload-video.js - commit 1

--- server/src/middlewares/upload-video.js
+++ server/src/middlewares/upload-video.js
@@ -4,3 +4,5 @@ const storage = multer.memoryStorage(); // store file in memory
 const upload = multer({ storage });
 
 export default upload;
+
+// Auto change for Fri Oct 25 2024 03:00:00 GMT+0300 (East Africa Time)
