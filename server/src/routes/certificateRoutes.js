a3ae2600e0500ed2bbfb00894f4ff01a88c48cc2
habtamubaye-devsec <habtamubaye2127gmail.com>
2024-10-31 07:02:00 +0000

    Update: src/routes/certificateRoutes.js - commit 2

--- server/src/routes/certificateRoutes.js
+++ server/src/routes/certificateRoutes.js
@@ -11,3 +11,5 @@ router.get("/my-certificates", protect, getMyCertificates);
 router.get("/:courseId/certificate", protect, generateCertificate);
 
 export default router;
+
+// Auto change for Thu Oct 31 2024 03:00:00 GMT+0300 (East Africa Time)
